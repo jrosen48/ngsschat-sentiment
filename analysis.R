7fa9e6a6870cd753f7bf19844a8dc4d8208f6538
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-06-26 11:06:05 -0400

    add data

--- analysis.R
+++ analysis.R
@@ -1,10 +1,34 @@
 library(tidyverse)
 library(osfr)
+library(lubridate)
+library(tidytags)
 
-# f <- osf_retrieve_file("https://osf.io/9ydz6/")
-# osf_download(f)
-# file.rename("ALL_NGSSchat_DATA_201109060000_TO_202006202334.rda", "all_ngsschat.rda")
+# uncomment the following lines of code to download the data
+
+f <- osf_retrieve_file("https://osf.io/9ydz6/")
+osf_download(f)
+file.rename("ALL_NGSSchat_DATA_201109060000_TO_202006202334.rda", "all_ngsschat.rda")
 
 load("all_ngsschat.rda")
 
-tweets_dl
+d <- tweets_dl %>%
+  mutate(screen_name = tolower(screen_name),
+         year = year(created_at),
+         day = round_date(created_at, "day"))
+
+d %>% 
+  count(screen_name) %>% # 16,328 participants
+  filter(n > 1) %>% # 6,065
+  filter(n >= 10) # 1,472
+
+d %>% 
+  count(year) %>% 
+  ggplot(aes(x = year, y = n)) +
+  geom_point() 
+
+d %>% 
+  count(day) %>% 
+  ggplot(aes(x = day, y = n)) +
+  geom_point()
+
+

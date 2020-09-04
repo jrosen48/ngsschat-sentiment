f69024342680ea0c800258833179988944bf5093
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2020-09-04 11:04:41 -0400

    first drake run

---
+++ r/functions.R
@@ -0,0 +1,4 @@
+load_rda <- function(f) {
+  load(f)
+  tweets_dl
+}
--- r/plan.R
+++ r/plan.R
@@ -1,9 +1,14 @@
 the_plan <-
   drake_plan(
 
-    loaded_rda_data = load(file_in("data-raw/data_final_2020_09_04.rda")),
-    state_data <- read_csv(file_in("data-raw/ngsschat-state-data.csv")),
+    loaded_rda_data = load_rda(file_in("data-raw/data_final_2020_09_04.rda")),
+    state_data = read_csv(file_in("data-raw/ngsschat-state-data.csv")),
     state_acronyms = read_csv(file_in("data-raw/state-acronyms.csv")),
     
-    
+    prepared_data = rmarkdown::render(
+      knitr_in("prepare-for-modeling.Rmd"),
+      output_file = file_out("docs/prepare-for-modeling.html"),
+      params = list(tweets_dl = loaded_rda_data,
+                    state_data = state_data,
+                    state_acronyms = state_acronyms))
 )

09308769ebbcd5f6ba6f2549a63f702d6f4c2e89
Joshua Rosenberg <jmrosen48@gmail.com>
2020-09-04 21:31:53 -0400

    remove acronyms; not used
    Former-commit-id: f049e39467ce910b8833b581e73f7d186437b4c9

--- r/plan.R
+++ r/plan.R
@@ -5,7 +5,6 @@ the_plan <-
     
     loaded_rda_data = load_rda(file_in("data-raw/data_final_2020_09_04.rda")),
     state_data = read_csv(file_in("data-raw/ngsschat-state-data.csv")),
-    state_acronyms = read_csv(file_in("data-raw/state-acronyms.csv")),
     
     # for RMD output
     
@@ -13,8 +12,7 @@ the_plan <-
       knitr_in("prepare-for-modeling.Rmd"),
       output_file = file_out("docs/prepare-for-modeling.html"),
       params = list(tweets_dl = loaded_rda_data,
-                    state_data = state_data,
-                    state_acronyms = state_acronyms)),
+                    state_data = state_data)),
     
     loaded_processed_data = read_rds(file_in("data/ngsschat-data-for-modeling.rds")),
     

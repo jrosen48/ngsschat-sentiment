467f79b5f518b9106bff7159c192b8d9cc6fbb8e
Joshua Rosenberg <jmrosen48@gmail.com>
2020-09-14 09:18:45 -0400

    major re-write of plan
    Former-commit-id: e026d21aea04928995a15ca1af588a5287192f7b

--- r/plan.R
+++ r/plan.R
@@ -1,36 +1,33 @@
 the_plan <-
   drake_plan(
-
+    
     # loading data
     
     loaded_rda_data = load_rda(file_in("data-raw/data_final_2020_09_09.rda")),
+    liwc_data = read_csv(file_in("data-raw/liwc-results.csv")),
     state_data = read_csv(file_in("data-raw/ngsschat-state-data.csv")),
     
+    # joining data
+    
+    loaded_rda_data_with_liwc = join_liwc(loaded_rda_data, liwc_data),
+    loaded_rda_data_with_liwc_with_state = join_state(loaded_rda_data_with_liwc, state_data),
+    
+    # preparing for modeling
+    
+    data_to_model = create_new_variables_and_filter_by_language(loaded_rda_data_with_liwc_with_state),
+    
     # for RMD output
-    
-    prepared_data = rmarkdown::render(
-      knitr_in("prepare-for-modeling.Rmd"),
-      output_file = file_out("docs/prepare-for-modeling.html"),
-      params = list(tweets_dl = loaded_rda_data,
-                    state_data = state_data)),
-    
-    loaded_processed_data = read_rds(file_in("data/ngsschat-data-for-modeling.rds")),
     
     descriptives = rmarkdown::render(
       knitr_in("describe-data.Rmd"),
       output_file = file_out("docs/descriptives.html"),
-      params = list(d = loaded_processed_data)),
+      params = list(d = data_to_model)),
     
-    fitted_models = rmarkdown::render(
-      knitr_in("model-sentiment.Rmd"),
-      output_file = file_out("docs/models.html"),
-      params = list(d = loaded_processed_data)),
-    
-    fitted_models_no_state = rmarkdown::render(
-      knitr_in("model-sentiment-no-state.Rmd"),
-      output_file = file_out("docs/models-without-states.html"),
-      params = list(d = loaded_processed_data)),
-    
+    # final_models = rmarkdown::render(
+    #   knitr_in("final-models.Rmd"),
+    #   output_file = file_out("docs/final-models.html"),
+    #   params = list(d = data_to_model)),
+  
     # for site
     
     dependencies = rmarkdown::render(
@@ -39,4 +36,4 @@ the_plan <-
     
     rendered_site = render_site()
     
-)
+  )

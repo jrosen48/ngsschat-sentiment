e8acc016dd49aaa7204ed56465f004d060c369e3
Joshua Rosenberg <jrosen@msu.edu>
2021-03-04 11:04:11 -0500

    results for using a non-scaled DV

--- r/functions.R
+++ r/functions.R
@@ -606,7 +606,7 @@ scale_key_vars <- function(d) {
            n_posted_chatsessions_s = as.numeric(scale(n_posted_chatsessions)),
            n_posted_ngsschat_nonchat_s = as.numeric(scale(n_posted_ngsschat_nonchat)),
            n_posted_non_ngsschat_s = as.numeric(scale(n_posted_non_ngsschat)),
-           senti_scale_s = as.numeric(scale(ss_scale, center = FALSE)))
+           senti_scale_s = ss_scale)
 }
 
 create_figure_1 <- function(d) {
